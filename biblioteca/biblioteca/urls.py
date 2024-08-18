fa94d09e50e3d78f96686515a83a32ef73596b42
Droid-M <62139366+Droid-M@users.noreply.github.com>
2024-08-17 23:41:56 -0300

    feat(add endpoints):

--- biblioteca/biblioteca/urls.py
+++ biblioteca/biblioteca/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
 
 urlpatterns = [
+    path('livros/', views.livro_list_create, name='livros-list-create'),
+    path('livros/<int:pk>/', views.livro_detail, name='livro-detail'),
     path('admin/', admin.site.urls),
 ]
